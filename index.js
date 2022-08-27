cf3a46e40135e28a52960b135f2612d3821029ea
H.Mohammad Owais <111707431+Dreambig7421@users.noreply.github.com>
2022-08-28 00:40:14 +0500

    ok

---
+++ index.js
@@ -0,0 +1,12 @@
+var allPera = document.getElementById("main-content");
+console.log(allPera);
+
+
+var fName = document.querySelector(".Alex");
+fName.value = "Alex";
+
+var lName = document.querySelector(".Bank");
+lName.value = "Bank";
+
+var email = document.querySelector(".Email");
+email.value = "Alexbank@example.com";
